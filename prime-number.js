0912ea1361a85684fd37720e13331b42dbb7c708
rakeshjella <rakeshjella123@gmail.com>
2025-03-31 17:37:46 +0530

    check number is prime

---
+++ prime-number.js
@@ -0,0 +1,12 @@
+function prime(n){
+    if(n<=1) return false;
+    for(var i=2;i<=Math.sqrt(n);i++){
+        if(n%i == 0) {
+            return false
+        }
+    }
+
+    return true
+}
+
+console.log(prime(31))
