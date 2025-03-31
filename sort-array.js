c9fab9874a2eb8bf0593516df39ac97284bccf30
rakeshjella <rakeshjella123@gmail.com>
2025-03-31 17:15:45 +0530

    sort array elements

---
+++ sort-array.js
@@ -0,0 +1,16 @@
+let arr = [1,4,2,8,7];
+
+function sortArr(arr){
+    for(var i=0; i<arr.length;i++){
+        for(var j=i;j<arr.length;j++){
+            if(arr[i]>arr[j]){
+                let temp = arr[i];
+                arr[i] =arr[j];
+                arr[j] = temp
+            }
+        }
+    }
+    return arr
+}
+
+console.log(sortArr(arr))
